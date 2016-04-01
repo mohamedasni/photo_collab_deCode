8a1f62b72b77805f369c79717d0930a31ec204ef
mohamedasni <asni.cas@gmail.com>
2016-04-01 17:21:22 -0400

    model functions

--- models/Project.js
+++ models/Project.js
@@ -3,9 +3,8 @@
  */
 var mongoose = require("mongoose");
 
-
-
 var ProjectSchema = new mongoose.Schema({
+    projectName: {type: String, required: true, unique: true},
     user: {type: String, required: true},
     annotation: [{
         user: {type: String, required: true},
@@ -21,6 +20,35 @@ var ProjectSchema = new mongoose.Schema({
     }]
 });
 
+ProjectSchema.statics.newProject = function(req, res){
+    var project = new this({
+        projectName: req.body.pName,
+        user: req.body.uName
+    });
+
+    project.save(function(err){
+        if(err){
+            if(err.code == 11000){
+                //this is if the project already exists error!
+                console.log("that project already exists!");
+                res.sendStatus(406);
+                return;
+            }
+
+            //if some other mongo weird error happens!
+            res.sendStatus(403);
+        }else{
+            console.log("project added successfully!");
+        }
+    });
+
+    addIniAnnotation(project, req);
+};
+
+ProjectSchema.methods.addIniAnnotation = function(project, req){
+    var data = {user: req.body.uName, img: req.body.img};
+    project.annotation.push(data);
+};
 
 module.exports = {
     model: mongoose.model('Project', ProjectSchema),
