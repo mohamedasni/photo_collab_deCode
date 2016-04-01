80ea9b108c91d7b81ddb72edebec7487e75bd36d
Phillip Williams <phillip.mitchel.williams@gmail.com>
2016-04-01 17:10:05 -0400

    Finalized api.js
    Finished API routes.

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,24 @@
-/*api.js*/
+/*api_post.js*/
+
 var Project = require('../models/Project');
 var mongoose = require('mongoose');
+
+app.post('/api/project',function (req, res) {
+	new Project.newProject(req,req);
+});
+
+app.get('/api/project',function (req, res) {
+	Project.findProject(req,res);
+});
+
+app.post('/api/annotations',function (req,res){
+	Project.addAnotation(req,res);
+});
+
+app.get('/api/annotations',function (req,res){
+	Project.findAnotation(req,res);
+});
+
+app.post('/api/comments',function (req,res){
+	Project.addComment(req,res);
+});
